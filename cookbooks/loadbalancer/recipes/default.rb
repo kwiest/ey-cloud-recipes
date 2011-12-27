61102b00ffdc995b94c000d00898d040b2fcc2b3
Adam Holt <me@adamholt.co.uk>
2011-12-27 15:41:57 +0000

    Don't need the kill-others recipe, as NGINX won't be running on this instance

--- cookbooks/loadbalancer/recipes/default.rb
+++ cookbooks/loadbalancer/recipes/default.rb
@@ -6,7 +6,6 @@ end
 # The install recipe expects to be called second
 # and will likely fail on clean instances if it's put first
 if node[:instance_role] == "util" && node[:name] == "loadbalancer"
-  require_recipe 'loadbalancer::kill-others'
   require_recipe 'loadbalancer::configure'
   require_recipe 'loadbalancer::install' unless node[:quick]
   require_recipe 'loadbalancer::ssl'

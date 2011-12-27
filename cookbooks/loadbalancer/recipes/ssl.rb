cd3f12156998f5bebd2f6d384814cf45e9134415
Adam Holt <me@adamholt.co.uk>
2011-12-27 15:39:41 +0000

    don't try to put the ssl cert there if it doesn't exist

--- cookbooks/loadbalancer/recipes/ssl.rb
+++ cookbooks/loadbalancer/recipes/ssl.rb
@@ -8,29 +8,33 @@ directory "/etc/nginx/ssl" do
   mode 0775
 end
 
-node[:applications].map {|k,v| [k,v] }.sort_by {|a,b| a }.each do |app, data|
-  template "/etc/nginx/ssl/#{app}.key" do
-    owner node[:owner_name]
-    group node[:owner_name]
-    mode 0644
-    source "sslkey.erb"
-    variables(
-      :key => data[:vhosts][1][:key]
-    )
-    backup 0
-    notifies :restart, resources(:service => "haproxy"), :delayed
-  end
+node.engineyard.environment.apps.sort_by {|a,b| a.name}.each do |app|
+  if app.https?
+    cert = app.vhosts.first.ssl_cert
 
-  template "/etc/nginx/ssl/#{app}.crt" do
-    owner node[:owner_name]
-    group node[:owner_name]
-    mode 0644
-    source "sslcrt.erb"
-    variables(
-      :crt => data[:vhosts][1][:crt],
-      :chain => data[:vhosts][1][:chain]
-    )
-    backup 0
-    notifies :restart, resources(:service => "haproxy"), :delayed
+    template "/etc/nginx/ssl/#{app.name}.key" do
+      owner node[:owner_name]
+      group node[:owner_name]
+      mode 0644
+      source "sslkey.erb"
+      variables(
+        :key => cert.private_key
+      )
+      backup 0
+      notifies :run, resources(:execute => 'reload-haproxy'), :delayed
+    end
+
+    template "/etc/nginx/ssl/#{app.name}.crt" do
+      owner node[:owner_name]
+      group node[:owner_name]
+      mode 0644
+      source "sslcrt.erb"
+      variables(
+        :crt => cert.certificate,
+        :chain => cert.certificate_chain
+      )
+      backup 0
+      notifies :run, resources(:execute => 'reload-haproxy'), :delayed
+    end
   end
 end

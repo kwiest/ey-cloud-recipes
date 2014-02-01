a200f7a4a7d8e4980a2f9a1e664757176478dca5
Ralph Bankston <ralph.l.bankston@gmail.com>
2014-01-31 16:02:37 -0800

    Removed openssh binary from repo and added magento and magento_redis

---
+++ cookbooks/magento_redis/recipes/default.rb
@@ -0,0 +1,26 @@
+#
+# Cookbook Name:: magento_redis
+# Recipe:: default
+#
+redis_instance = node['utility_instances'].find { |instance| instance['name'] == 'redis' }
+app_name = "magtest" 
+key_name = '0e0a307224ae08c01ab13c1d15581d6c'
+
+if ['solo', 'app', 'app_master'].include?(node[:instance_role])
+    template "/data/#{app_name}/current/app/etc/local.xml" do
+    owner node[:owner_name]
+    group node[:owner_name]
+    mode 0644
+    source "local.xml.erb"
+    variables({
+      :app_name => app_name,
+      :dbuser => node[:owner_name],
+      :dbpass => node[:owner_pass],
+      :dbhost => node[:db_host],
+      :key => key_name,
+      :hostname => redis_instance[:hostname]
+    })
+
+  end
+  
+end

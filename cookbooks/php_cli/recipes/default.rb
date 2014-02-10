abf8a1ee6edec519ac5e5820617858e7304935db
Ralph Bankston <ralph.l.bankston@gmail.com>
2014-02-10 17:53:31 -0500

    Added php-cli to my repo

---
+++ cookbooks/php_cli/recipes/default.rb
@@ -0,0 +1,15 @@
+template "/home/deploy/.bashrc" do
+    owner node[:owner_name]
+    group node[:owner_name]
+    mode 0644
+    source ".bashrc.erb"
+    variables({
+      :app_name => app_name,
+      :php_env => node[:environment][:framework_env],
+      :user => node[:owner_name],
+      :dbuser => node[:owner_name],
+      :dbpass => node[:owner_pass],
+      :dbhost => node[:db_host],
+    })
+    not_if "grep DB_HOST /home/deploy/.bashrc"
+  end

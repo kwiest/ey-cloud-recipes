7fa7361d94bf6716967b8f92c7ee5326d4045f6a
Ralph Bankston <ralph.l.bankston@gmail.com>
2014-07-18 14:37:32 -0400

    Need App_name for the db

--- cookbooks/php_cli/recipes/default.rb
+++ cookbooks/php_cli/recipes/default.rb
@@ -1,9 +1,13 @@
+#app_name is your applications name to get the proper database field set
+app_name = "Put Your Application's Name Here"
+
 template "/home/#{node[:owner_name]}/.bashrc" do
     owner node[:owner_name]
     group node[:owner_name]
     mode 0644
     source ".bashrc.erb"
     variables({
+      :app_name => app_name,
       :php_env => node[:environment][:framework_env],
       :user => node[:owner_name],
       :dbuser => node[:owner_name],
